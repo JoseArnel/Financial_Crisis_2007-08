d147b85319b2302054ff64e0a7bfce783dc7c049
Jose Arnel <jonel.manipon@gmail.com>
2024-08-19 15:43:41 -0400

    aug19

--- Practical Statistics.py
+++ Practical Statistics.py
@@ -21,3 +21,32 @@ ax.set_ylabel('Count')
 etfs = sp500_px.loc[sp500_px.index > '2012-07-01', sp500_sym[sp500_sym['sector'] == 'etf'] ['symbol']]
 #seaborn.heatmap package
 sns.heatmap(etfs.corr(), vmin=-1, vmax=1), cmap=sns.diverging_palette(20, 220, as_cmap=True)
+
+# ex: Scatterplots, pg 34
+ax = telecom.plot.scatter(x='T', y='VZ', figsize=(4,4), marker='$\u25EF$')
+ax.set_xlabel('ATT (T')
+ax.set_ylabel('Verizon (VZ)')
+ax.axhline(0, color='grey', lw=1)
+ax.axvline(0, color='grey', lw=1)
+
+#ex: Histogram of Annual Incomes of 1,000 laon applicants
+import pandas as pd
+import seaborn as sns
+
+sample_data = pd.DataFrame({
+    'income': loans_income.sample(1000),
+    'type': 'Data',
+})
+sample_mean_05 = pd.DataFrame({
+    'income' : [laons_income.sample(5).mean() for _ in range(1000)],
+    'type' : 'Mean of 5',
+})
+sample_mean_20 = pd.DataFrame({
+    'income' :[loans_income.sample(20).mean() for _ in range(1000)],
+    'type' : 'Mean of 20',
+})
+results = pd.concat([sample_data, sample_mean_05, sample_mean_20])
+g = sns.FacetGrid(results, col='type', col_wrap=1, height=2, aspect=2)
+g.map(plt.hist,'income', range[0,20000], bins=40)
+g.set_axis_laels('Income', 'Count')
+g.set_titles('{col_name}')
